d82763f2cab8b4e75c8a87d6a1c18b6fcd07df27
Uriel Solis Salinas <euss99@hotmail.com>
2025-06-11 12:33:42 -0600

    config: 🚀 initialize React + TypeScript + Vite with ESLint and Tailwind CSS

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import globals from "globals"
+import js from "@eslint/js"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import tseslint from "typescript-eslint"
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "no-duplicate-imports": "error",
+      "no-unused-expressions": "error",
+      "no-unused-vars": "off",
+      "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": "error",
+      indent: ["error", 2],
+      quotes: ["error", "double"],
+    },
+  },
+)
